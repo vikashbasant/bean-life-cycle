6a2e0c93dce3d401cf33f1f2968fe2d0a3f7a3a2
Vikash Basant <basantvikash360@gmail.com>
2023-07-06 10:20:32 +0530

    learning spring bean life cycle

---
+++ src/main/java/com/kash/beanlifecycle/BeanLifeCycleApplication.java
@@ -0,0 +1,13 @@
+package com.kash.beanlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanLifeCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanLifeCycleApplication.class, args);
+	}
+
+}
